aec1886c8c08faa87100ed41b5aa939632bede21
Admin <aqilzeka99@gmail.com>
2020-08-23 22:54:37 +0400

    Initial commit

---
+++ spring-boot-microservice-forex-service/src/main/java/com/example/forexservice/api/ForexController.java
@@ -0,0 +1,29 @@
+package com.example.forexservice.api;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Objects;
+
+@RestController
+@RequiredArgsConstructor
+public class ForexController {
+
+    private final Environment environment;
+    private final ExchangeValueRepository exchangeValueRepository;
+
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    public ExchangeValue retrieveExchangeValue(@PathVariable String from,
+                                               @PathVariable String to) {
+        ExchangeValue exchangeValue = exchangeValueRepository.findByFromAndTo(from, to).orElseThrow(
+                () -> { throw new ExchangeValueNotFound("Exchange Value Not Found"); }
+        );
+
+        exchangeValue.setPort(Integer.parseInt(Objects.requireNonNull(environment.getProperty("local.server.port"))));
+
+        return exchangeValue;
+    }
+}
