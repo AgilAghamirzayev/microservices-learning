a8d9892f631935e530ff95d17e9731f06fa1646c
Admin <aqilzeka99@gmail.com>
2020-08-24 23:37:39 +0400

    currency-conversion

---
+++ currency-exchange-microservice/spring-boot-microservice-forex-service/src/main/java/com/example/forexservice/api/ExchangeValueRepository.java
@@ -0,0 +1,9 @@
+package com.example.forexservice.api;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ExchangeValueRepository extends JpaRepository<ExchangeValue, Long> {
+    Optional<ExchangeValue> findByFromAndTo(String from, String to);
+}
---
+++ currency-exchange-microservice/spring-boot-microservice-forex-service/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into exchange_value(currency_from, currency_to, conversion_multiple, port) values('USD', 'AZN', 65, 0);
+ -- insert into exchange_value(currency_from, currency_to, conversion_multiple, port) values('USD', 'AZN', 75, 0);
+ -- insert into exchange_value(currency_from, currency_to, conversion_multiple, port) values('USD', 'AZN', 25, 0);
