452763a688aabec123af7cd9fa50820c95573c02
Admin <aqilzeka99@gmail.com>
2020-08-24 23:27:50 +0400

    Added currency-converter

---
+++ currency-exchange-microservice/spring-boot-microservice-currency-conversion/src/main/java/com/example/currencyconversion/api/CurrencyConversionBean.java
@@ -0,0 +1,22 @@
+package com.example.currencyconversion.api;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CurrencyConversionBean {
+
+    private Long id;
+    private String from;
+    private String to;
+    private BigDecimal conversionMultiple;
+    private BigDecimal quantity;
+    private BigDecimal totalCalculatedAmount;
+    private int port;
+
+}
--- spring-boot-microservice-forex-service/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-insert into exchange_value(currency_from, currency_to, conversion_multiple, port) values('USD', 'AZN', 65, 0);
-insert into exchange_value(currency_from, currency_to, conversion_multiple, port) values('USD', 'AZN', 75, 0);
-insert into exchange_value(currency_from, currency_to, conversion_multiple, port) values('USD', 'AZN', 25, 0);
