6885657fa40af5f417960b057338857d9942fc27
Admin <aqilzeka99@gmail.com>
2020-08-25 23:27:00 +0400

    Added eureka naming service

--- currency-exchange-microservice/spring-boot-microservice-currency-conversion/src/main/java/com/example/currencyconversion/api/CurrencyConversionController.java
+++ currency-exchange-microservice/spring-boot-microservice-currency-conversion/src/main/java/com/example/currencyconversion/api/CurrencyConversionController.java
@@ -1,34 +1,44 @@
 package com.example.currencyconversion.api;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
-import java.util.HashMap;
-import java.util.Map;
 
 @Log4j2
 @RestController
+@RequiredArgsConstructor
 public class CurrencyConversionController {
 
+    private final CurrencyExchangeServiceProxy proxy;
 
+    //    @GetMapping("/currency-converter/from/{from}/to/{to}/quantity/{quantity}")
+//    public CurrencyConversionBean convertCurrency(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {
+//
+//        Map<String, String> uriVariables = new HashMap<>();
+//        uriVariables.put("from", from);
+//        uriVariables.put("to", to);
+//
+//        ResponseEntity<CurrencyConversionBean> responseEntity = new RestTemplate().getForEntity(
+//                "http://localhost:8000/currency-exchange/from/{from}/to/{to}", CurrencyConversionBean.class, uriVariables);
+//
+//        CurrencyConversionBean body = responseEntity.getBody();
+//
+//        return new CurrencyConversionBean(body.getId(), from, to, body.getConversionMultiple(), quantity,
+//                quantity.multiply(body.getConversionMultiple()), body.getPort());
+//
+//    }
     @GetMapping("/currency-converter/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversionBean convertCurrency(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {
-        Map<String, String> uriVariables = new HashMap<>();
-        uriVariables.put("from", from);
-        uriVariables.put("to", to);
 
-        ResponseEntity<CurrencyConversionBean> responseEntity = new RestTemplate().getForEntity(
-                "http://localhost:8000/currency-exchange/from/{from}/to/{to}", CurrencyConversionBean.class, uriVariables);
+        CurrencyConversionBean response = proxy.retrieveExchangeValue(from, to);
 
-        CurrencyConversionBean body = responseEntity.getBody();
-
-        return new CurrencyConversionBean(body.getId(), from, to, body.getConversionMultiple(), quantity,
-                quantity.multiply(body.getConversionMultiple()), body.getPort());
+        log.info("{}", response);
+        return new CurrencyConversionBean(response.getId(), from, to, response.getConversionMultiple(), quantity,
+                quantity.multiply(response.getConversionMultiple()), response.getPort());
 
     }
 }
---
+++ currency-exchange-microservice/spring-boot-microservice-eureka-naming-server/src/test/java/com/example/eurekanamingserverdemo/EurekaNamingServerdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eurekanamingserverdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaNamingServerdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
